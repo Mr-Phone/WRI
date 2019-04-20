d931b720a0c0c7747e955fd2b604a5e8bb7fcf13
Double <Mr-Phone@users.noreply.github.com>
2019-04-20 09:02:51 +0800

    Update weibo.js

--- common/weibo.js
+++ common/weibo.js
@@ -218,6 +218,8 @@ function formatStatus(status, largePic = true, emoji = false) {
   temp = temp.replace(/<a data-url=(.*?)><span class='url-icon'><img style='width: 1rem;height: 1rem' src='https:\/\/h5.sinaimg.cn\/upload\/2015\/09\/25\/3\/timeline_card_small_location_default.png'><\/span>/g,'<br><a data-url=$1>📍');
   //购物车图标处理
   temp = temp.replace(/<a data-url=(.*?)><span class='url-icon'><img style='width: 1rem;height: 1rem' src='https:\/\/h5.sinaimg.cn\/upload\/2015\/01\/21\/20\/timeline_card_small_photo_default.png'><\/span>/g,'<br><a data-url=$1>');
+  //超话图标处理
+  temp = temp.replace(/<a data-url=(.*?)><span class='url-icon'><img style='width: 1rem;height: 1rem' src='https:\/\/n.sinaimg.cn\/photo\/5213b46e\/20180926\/timeline_card_small_super_default.png'><\/span>/g,'<br><a data-url=$1>💎');
   //表情转文字
   temp = temp.replace(/<span class="url-icon"><img alt=(.*?) src="(.*?)" style="width:1em; height:1em;" \/><\/span>/g,' $1 ');
   
